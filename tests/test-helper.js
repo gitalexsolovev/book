fd6f084adeb6a4da1a3603210e635020eb277a8e
gitalexsolovev <solovey1595@gmail.com>
2020-10-12 21:21:01 +0500

    commit 1
    1

--- tests/test-helper.js
+++
@@ -1,8 +0,0 @@
-import Application from 'book/app';
-import config from 'book/config/environment';
-import { setApplication } from '@ember/test-helpers';
-import { start } from 'ember-qunit';
-
-setApplication(Application.create(config.APP));
-
-start();
